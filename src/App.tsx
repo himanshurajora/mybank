144b7180ca2edad19d3968c985164a4a51a22140
Himanshu Jangid <iamahalofan@gmail.com>
2021-08-03 21:50:22 +0530

    database creation and browser support

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
@@ -9,6 +9,23 @@ import data from './dataset/dataset'
 
 function App() {
   const [count, setCount] = useState(0)
+  var dataBase : Database;
+  useEffect(() => {
+    if(!window.openDatabase){
+      alert("Your Browser Doesn't Support WebSQL Please Use Chrome, Brave, Edge Chromium or Any Chromium Based Browser | \n | WebSQL Doesn't Work on FireFox, Internet Explorer and Old Edge Browser")
+    }
+    else{
+      try{
+        dataBase = window.openDatabase("DemoWebSql", "1.0", "Test DataBase", 1024 * 1024 * 2, () => {
+          console.log("dataBase Created")
+        })
+      }
+      catch(err){
+        console.log(err);
+        
+      }
+    }
+  })
 
   return (
     <div className="App">
