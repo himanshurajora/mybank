918f402de5b28adcdb4ca3eddcd1f8b7d63916b8
Himanshu Jangid <iamahalofan@gmail.com>
2021-08-03 21:27:47 +0530

    all customers stylesheet

---
+++ src/components/All.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import "./Navbar.css"
+function All() {
+    return (
+        <div>
+            This is for all of us
+        </div>
+    )
+}
+
+export default All
