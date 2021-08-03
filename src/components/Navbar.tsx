fd2d2351b4bfd62c7bbc86b8ae8395737fc2c6c3
Himanshu Jangid <iamahalofan@gmail.com>
2021-08-03 21:26:57 +0530

    navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import "./Navbar.css"
+function Navbar() {
+    return (
+        <div>
+            <nav>
+                <Link to="/" className="main-link">MyBank</Link>
+                <span>
+                {/* <Link to="/" className="link">Home</Link> */}
+                <Link to="/customers" className="link">View All Customers</Link>
+                </span>
+            </nav>
+        </div >
+    )
+}
+
+export default Navbar
