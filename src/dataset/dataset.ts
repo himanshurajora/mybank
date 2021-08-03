c09c32d9d47358765ced80a2eb9cdf74ee1dfbda
Himanshu Jangid <iamahalofan@gmail.com>
2021-08-03 21:27:52 +0530

    init

---
+++ src/dataset/dataset.ts
@@ -0,0 +1,17 @@
+var Customers = [
+    {
+        "name":"Himanshu"
+    },
+    {
+        "name":"Ankit"
+    }
+]
+
+var Transactions = [
+    {
+        "data" : 12
+    }
+]
+
+
+export default {Customers, Transactions}
